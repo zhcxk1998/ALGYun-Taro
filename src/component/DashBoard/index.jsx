dd7d0def3f4a166c8e1c91655dce0ea29e9df9c5
zhcxk1998 <464203147@qq.com>
2019-04-29 23:42:57 +0800

    feat: 优化了个人中心模块.

--- src/component/DashBoard/index.jsx
+++ src/component/DashBoard/index.jsx
@@ -1,9 +1,11 @@
 import Taro, { Component } from '@tarojs/taro';
 import { View, Text, Image, Button } from '@tarojs/components';
 import { AtGrid, AtList, AtListItem } from "taro-ui";
+import { inject } from '@tarojs/mobx';
 
 import './style.css';
 
+@inject('userStore')
 class DashBoard extends Component {
   config = {
     navigationBarTitleText: '个人中心',
@@ -12,25 +14,43 @@ class DashBoard extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      // isLogin: false
     }
   }
 
+  handleClick = () => {
+    Taro.navigateTo({
+      url: '/pages/entry/index'
+    })
+  }
+
+  back = () => {
+    const { userStore } = this.props;
+    userStore.handleLogin(false)
+    Taro.reLaunch({
+      url: '/pages/index/index'
+    }).then(() => {
+      userStore.handleChange(4)
+    })
+  }
+
   render() {
+    const { isLogin } = this.props.userStore;
     return (
       <View>
-        {/* <View style={style} >
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map(item => (
-            <View>{item}</View>
-          ))}
-        </View> */}
-        {/* <View>{userStore.username}</View> */}
         <View className='dashBoard'>
-          <View className='infomation'>
-            <Image src='http://cdn.algbb.fun/emoji/32.png' className='avater' />
-            <Text className='userName'>BB</Text>
-            <Text className='info'>我是一名好学生，我的爱好是打游戏，不爱学习</Text>
-          </View>
+          {
+            !isLogin ?
+              <View className='infomation'>
+                <Button className='login-button' onClick={this.handleClick}>立即登录</Button>
+              </View>
+              :
+              <View className='infomation'>
+                <Image src='http://cdn.algbb.fun/emoji/32.png' className='avater' />
+                <Text className='userName'>BB</Text>
+                <Text className='info'>我是一名好学生，我的爱好是打游戏，不爱学习</Text>
+              </View>
+          }
           <View className='grid'>
             <AtGrid onClick={this.gridClick} mode='rect' hasBorder data={
               [
@@ -71,7 +91,7 @@ class DashBoard extends Component {
               />
             </AtList>
           </View>
-          <Button className='quit' onClick={this.back}>退出登录</Button>
+          <Button className='quit' onClick={this.back} hidden={!isLogin}>退出登录</Button>
         </View>
       </View>
     )
--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,9 +1,18 @@
 import { observable } from 'mobx'
 
 const userStore = observable({
+  isLogin: false,
   username: 'wadaaw',
+  current: 0,
+  handleChange(current) {
+    this.current = current
+  },
+  handleLogin(status) {
+    this.isLogin = status
+  },
   setName(name) {
     this.username = name;
   }
+
 })
 export default userStore
