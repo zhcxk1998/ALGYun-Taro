64f4aa89b444c90451a39180fc9b224604c6755b
zhcxk1998 <464203147@qq.com>
2019-04-29 11:52:27 +0800

    chore: 新增mock商品信息.

---
+++ src/utils/mockCommodity.js
@@ -0,0 +1,11 @@
+module.exports = [
+  'http://img13.360buyimg.com/n7/jfs/t6082/110/283779492/177938/4777ed9d/59278aa7N6184a2a7.jpg',
+  'http://img13.360buyimg.com/n7/jfs/t1/16741/28/1641/50475/5c145d1aE55defacb/3c3b1780efe61fee.jpg',
+  'http://img11.360buyimg.com/n7/jfs/t18877/107/2668951699/70961/f7fa694f/5b07755cNf281e1b9.jpg',
+  'http://img12.360buyimg.com/n7/jfs/t1/2998/14/5866/251074/5ba0cae1E3050fbea/a073ed8f69ab937c.jpg',
+  'http://img10.360buyimg.com/n7/jfs/t27859/324/1839173135/189759/c7ef4f/5bf7a308Nb2abf83f.jpg',
+  'http://img13.360buyimg.com/n7/jfs/t1/32558/10/14219/141360/5cbd3773E9c839949/4d1cfd102380fa0e.jpg',
+  'http://img14.360buyimg.com/n7/jfs/t19735/360/1799615030/232700/57874185/5ad9437eN0c5e011c.jpg',
+  'http://img13.360buyimg.com/n7/jfs/t1/11655/3/6359/22427/5c3ec370E9845191e/8fddaa1f4ae1b283.jpg',
+  'http://img10.360buyimg.com/n7/jfs/t25351/347/476263960/67434/f980f78c/5b70fedcN3230839a.jpg',
+]
